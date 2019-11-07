212c453951c33ffeaa7c723b6c5ec3da6f68f1d9
Jeroen Mandersloot <jeroen.mandersloot@gmail.com>
2019-11-07 18:33:05 +0100

    aap

--- generate_samples.py
+++ generate_samples.py
@@ -21,18 +21,22 @@ def generate_mixture(allele_pairs, mean_rfus):
     return np.sum(persons, axis=0)
 
 
-def generate_samples(n, is_binary, n_contributors=3, threshold = 50):
-    res = np.zeros((n, len(freqs) * 2))
+def generate_samples(n, is_binary, n_contributors=3, threshold=50, n_samples=2):
+    res = np.zeros((n, len(freqs) * n_samples))
     for i in range(n):
-        allele_pairs = [generate_alleles() for _ in range(2*n_contributors)]
-        mean_rfus = np.random.randint(50, 1000, 2*n_contributors)
+        allele_pairs = [generate_alleles() for _ in range(n_samples*n_contributors)]
+        mean_rfus = np.random.randint(50, 1000, n_samples*n_contributors)
         res[i, :len(freqs)] = generate_mixture(allele_pairs[:n_contributors], mean_rfus[:n_contributors])
         if i < n / 2:
-            res[i, len(freqs):] = generate_mixture(
-                allele_pairs[n_contributors:-1] + [allele_pairs[0]], mean_rfus[n_contributors:])
+            res[i, len(freqs):len(freqs)*2] = generate_mixture(
+                allele_pairs[n_contributors:2*n_contributors-1] + [allele_pairs[0]], mean_rfus[n_contributors:2*n_contributors])
+            res[i, len(freqs)*2:] = generate_mixture(
+                allele_pairs[n_contributors*2:-1] + [allele_pairs[0]], mean_rfus[2*n_contributors:])
         else:
-            res[i, len(freqs):] = generate_mixture(
-                allele_pairs[n_contributors:], mean_rfus[n_contributors:])
+            res[i, len(freqs):len(freqs)*2] = generate_mixture(
+                allele_pairs[n_contributors:2*n_contributors], mean_rfus[n_contributors:2*n_contributors])
+            res[i, len(freqs)*2:] = generate_mixture(
+                allele_pairs[2*n_contributors:], mean_rfus[2*n_contributors:])
     y = np.array([1] * int(n / 2) + [0] * int(n / 2))
     if is_binary:
         return 1*(res>threshold), y
@@ -41,6 +45,6 @@ def generate_samples(n, is_binary, n_contributors=3, threshold = 50):
 
 if __name__ == '__main__':
     allele_pairs = [generate_alleles() for _ in range(3)]
-    X, y  = generate_samples(10, False, 1)
+    X, y  = generate_samples(10, False, 1, n_samples=3)
     binary = 1 * (X > 50)
-    print(X)
+    print(X.shape)
