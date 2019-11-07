c6ff3753007855863a5707ca8027d1c62f9a0a2f
Timo Matzen <timomatzen87@gmail.com>
2019-11-07 11:44:06 +0100

    updates baseline

--- Models.py
+++ Models.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def baseline(df, threshold=5):
-    # Welke features zijn hetzelfde
-    same = df[:, 0:10] == df[:, 10:]
 
-    # sommeer over dezelfde features
-    sum_features = np.sum(same, axis=0)
+def baseline(numpy_data):
+    same = numpy_data[:, 0:10] == numpy_data[:, 10:]
 
-    # wanneer boven vijf return 1
-    predictions = sum_features > threshold
+    sum_feature = np.sum(same, axis=0)
+    zero_feature = np.sum(numpy_data, axis=0)
+    one_feature = np.sum(numpy_data == 1, axis=0)
 
-    return(predictions)
+    features = np.concatenate((sum_feature, zero_feature, one_feature), axis=1)
+
+    return (features)
