b15fc6eb87b41c3aa698f228d62c37d58e0b447c
navanhalem <na.vanhalem@gmail.com>
2019-11-07 15:25:07 +0100

    added multiple loci

--- cnn.py
+++ cnn.py
@@ -2,28 +2,27 @@ import numpy as np
 import generate_samples as gen
 
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, LeakyReLU
+from keras.layers import Dense, Conv1D, Conv2D, Flatten, Dropout, MaxPooling2D, LeakyReLU
 from keras.utils import to_categorical
 
+from sklearn.metrics import confusion_matrix
+
 def prepare_data(n):
-    data = gen.generate_samples(n, True)
+    data = gen.generate_samples(n, True, n_contributors=3)
     X = np.reshape(data[0], (n, 2, 10, 1))
     y = data[1]
     y = to_categorical(y)
     return X, y
 
 def make_model():
-    global model
     model = Sequential()
-    model.add(Conv2D(16, kernel_size=2, padding='same', input_shape=(2, 10, 1)))
+    model.add(Conv2D(32, kernel_size=2, padding='same', input_shape=(2, 10, 1)))
     model.add(LeakyReLU())
     model.add(Conv2D(8, kernel_size=2, padding='same'))
-    model.add(LeakyReLU())
-    model.add(MaxPooling2D(2))
     model.add(Flatten())
-    model.add(Dense(16))
+    model.add(Dense(256))
     model.add(LeakyReLU())
-    model.add(Dense(8))
+    model.add(Dense(64))
     model.add(LeakyReLU())
     model.add(Dense(2, activation='softmax'))
     return model
@@ -32,7 +31,8 @@ X_train, y_train = prepare_data(10000)
 X_test, y_test = prepare_data(1000)
 
 model = make_model()
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-model.fit(X_train, y_train, batch_size=512, epochs=20)
-print(model.test_on_batch(X_test, y_test))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(X_train, y_train, batch_size=256, epochs=100)
 
+print(confusion_matrix(np.argmax(y_test, axis=1), np.argmax(model.predict(X_test, verbose=0), axis=1)))
+print(model.test_on_batch(X_test, y_test))
--- generate_samples.py
+++ generate_samples.py
@@ -7,32 +7,32 @@ def generate_alleles():
     return np.random.choice(len(freqs), 2, p=freqs)
 
 
-def generate_person(allele_pair, mean_rfu):
+def generate_person(allele_pair, mean_rfu, l):
     heights = np.random.gamma(mean_rfu * 10, 1 / 10, 2)
-    res = np.zeros((len(freqs, )))
+    res = np.zeros((len(freqs, )*l))
     for i, allele in enumerate(allele_pair):
         res[allele] += heights[i]
     return res
 
 
-def generate_mixture(allele_pairs, mean_rfus):
-    persons = [generate_person(allele_pair, mean_rfu) for allele_pair, mean_rfu
+def generate_mixture(allele_pairs, mean_rfus, l=1):
+    persons = [generate_person(allele_pair, mean_rfu, l) for allele_pair, mean_rfu
                in zip(allele_pairs, mean_rfus)]
     return np.sum(persons, axis=0)
 
 
-def generate_samples(n, is_binary, n_contributors=3):
-    res = np.zeros((n, len(freqs) * 2))
+def generate_samples(n, is_binary, n_contributors=3, l=1):
+    res = np.zeros((n, len(freqs) * 2 * l))
     for i in range(n):
         allele_pairs = [generate_alleles() for _ in range(2*n_contributors)]
         mean_rfus = np.random.randint(50, 1000, 2*n_contributors)
-        res[i, :len(freqs)] = generate_mixture(allele_pairs[:n_contributors], mean_rfus[:n_contributors])
+        res[i, :len(freqs)*l] = generate_mixture(allele_pairs[:n_contributors], mean_rfus[:n_contributors], l)
         if i < n / 2:
-            res[i, len(freqs):] = generate_mixture(
-                allele_pairs[n_contributors:-1] + [allele_pairs[0]], mean_rfus[n_contributors:])
+            res[i, len(freqs)*l:] = generate_mixture(
+                allele_pairs[n_contributors:-1] + [allele_pairs[0]], mean_rfus[n_contributors:], l)
         else:
-            res[i, len(freqs):] = generate_mixture(
-                allele_pairs[n_contributors:], mean_rfus[n_contributors:])
+            res[i, len(freqs)*l:] = generate_mixture(
+                allele_pairs[n_contributors:], mean_rfus[n_contributors:], l)
     y = np.array([1] * int(n / 2) + [0] * int(n / 2))
     if is_binary:
         return 1*(res>50), y
@@ -41,6 +41,6 @@ def generate_samples(n, is_binary, n_contributors=3):
 
 if __name__ == '__main__':
     allele_pairs = [generate_alleles() for _ in range(3)]
-    X, y  = generate_samples(10, False, 1)
+    X, y  = generate_samples(1000, True, 1)
     binary = 1 * (X > 50)
     print(X)
