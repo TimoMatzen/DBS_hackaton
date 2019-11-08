ff24fdf70a69009be1e74accf1cf666905286e80
Jeroen Mandersloot <jeroen.mandersloot@gmail.com>
2019-11-08 13:41:42 +0100

    updates

--- fit_models.py
+++ fit_models.py
@@ -1,63 +1,104 @@
+import random
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model.logistic import LogisticRegression
-from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
-from Models import baseline, diff_features
-from generate_samples import generate_samples
-from matplotlib import pyplot as plt
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 
-n_samples = 100000
-n_contributors = 2
+from Models import calc_prob_multiple_samples, diff_features
+from generate_samples import generate_samples, generate_multiple_samples
+
+n_samples = 10000
+n_contributors = 3
+threshold = 50
+
+thresholds = list(range(0, 501, 50))
+num_loci = list(range(1, 21))
+
+results = defaultdict(dict)
+
 if __name__ == "__main__":
-    shuffle_idx = np.random.choice(np.arange(0, n_samples, 1), replace=False, size=n_samples)
+    for n in num_loci:
+        np.random.seed(42)
+        random.seed(42)
 
-    # generate binary data
-    X_train, y_train = generate_samples(n_samples, True, threshold=0, n_contributors=n_contributors)
-    X_train, y_train = X_train[shuffle_idx, :], y_train[shuffle_idx]
+        shuffle_idx = np.random.choice(np.arange(0, n_samples, 1),
+                                       replace=False, size=n_samples)
 
-    X_test, y_test = generate_samples(1000, True, threshold=0, n_contributors=n_contributors)
+        # generate binary data
+        X_train, y_train = generate_samples(n_samples, True,
+                                            threshold=threshold,
+                                            n_contributors=n_contributors)
+        X_train, y_train = X_train[shuffle_idx, :], y_train[shuffle_idx]
 
-    diff_X_train = diff_features(X_train)
-    diff_X_test = diff_features(X_test)
-    # create features
-    baseline_X_train = baseline(X_train)
-    baseline_X_test = baseline(X_test)
+        X_test, y_test = generate_samples(1000, True, threshold=threshold,
+                                          n_contributors=n_contributors)
 
-    # for model in [('Logistic: ', LogisticRegression()), ('RF: ', RandomForestClassifier()), ('SVM: ', SVC())]:
-    #
-    #     # mod = model[1]
-    #     # mod.fit(X_train, y_train)
-    #     # y_hat = mod.predict(X_test)
-    #     #
-    #     # print("Accuracy normal data", model[0], np.mean(y_hat == y_test))
-    #     #
-    #     # mod = model[1]
-    #     # mod.fit(baseline_X_train, y_train)
-    #     # y_hat = mod.predict(baseline_X_test)
-    #     #
-    #     # print("Accuracy baseline data", model[0], np.mean(y_hat == y_test))
-    #
-    #     mod = model[1]
-    #     # mod.fit(diff_X_train, y_train)
-    #     # y_hat = mod.predict(diff_X_test)
-    #     #
-    #     GridSearchCV(mod, parameters)
-    #
-    #     print("Accuracy diff data", model[0], np.mean(y_hat == y_test))
-    accuracies = []
-    sample_sizes = np.logspace(2, np.log10(n_samples), num=10)
-    mod = LogisticRegression()
-    mod.fit(baseline_X_train, y_train)
-    y_hat = mod.predict(baseline_X_test)
-    print(np.mean(y_test== y_hat))
-    # for size in sample_sizes:
-    #     print(size)
-    #     mod = RandomForestClassifier()
-    #     mod.fit(diff_X_train[:int(size), :], y_train[:int(size)])
-    #     y_hat = mod.predict_proba(diff_X_test)
-    #
-    #     accuracies.append(np.mean(y_hat == y_test))
+        X_extra = generate_multiple_samples(1000, True, threshold=threshold,
+                                            n_contributors=n_contributors,
+                                            n_loci=n)
 
+        diff_X_train = diff_features(X_train)
+        diff_X_test = diff_features(X_test)
 
-    plt.scatter(sample_sizes, accuracies)
-    plt.show()
+        # create features
+        # baseline_X_train = baseline(X_train)
+        # baseline_X_test = baseline(X_test)
+
+        for model in [('Logistic', LogisticRegression()),
+                      ('RF', RandomForestClassifier()),
+                      ('SVC', SVC(probability=True))]:
+            # mod = model[1]
+            # mod.fit(X_train, y_train)
+            # y_hat = mod.predict(X_test)
+            # accuracy_normal = np.mean(y_hat == y_test)
+            # print("Accuracy normal data", model[0], accuracy_normal)
+
+            # mod = model[1]
+            # mod.fit(baseline_X_train, y_train)
+            # y_hat = mod.predict(baseline_X_test)
+            # accuracy_baseline = np.mean(y_hat == y_test)
+            # print("Accuracy baseline data", model[0], accuracy_baseline)
+
+            mod = model[1]
+            mod.fit(diff_X_train, y_train)
+            # y_hat = mod.predict(diff_X_test)
+            # accuracy_diff = np.mean(y_hat == y_test)
+            # print("Accuracy diff data", model[0], accuracy_diff)
+
+            # results[model[0]][threshold] = dict(
+            #     normal=accuracy_normal,
+            #     # baseline=accuracy_baseline,
+            #     # diff=accuracy_diff
+            # )
+
+            y_hat_extra = np.mean(calc_prob_multiple_samples(X_extra, model[1]),
+                                  axis=0)
+
+            accuracy = accuracy_score(y_test,
+                                      [1 if m > .5 else 0 for m in y_hat_extra])
+            results[model[0]][n] = dict(diff=accuracy)
+
+        # baseline model
+        # y_baseline_1 = (baseline_X_test[:, 0] < np.mean(baseline_X_train[:, 0])) * 1
+        # baseline_acc_1 = np.mean(y_baseline_1 == y_test)
+        # results['Baseline'][threshold] = dict(baseline=baseline_acc_1)
+
+fig, ax = plt.subplots(1, 1)
+ax.set_xlabel('#Loci')
+ax.set_ylabel('Accuracy')
+for model, r1 in results.items():
+    thresholds = list(r1.keys())
+    for feature_type in ['normal', 'diff', 'baseline']:
+        try:
+            accuracies = [k[feature_type] for k in r1.values()]
+            line, *_ = ax.plot(thresholds, accuracies)
+            line.set_label(f'{model} ({feature_type})')
+        except (KeyError, IndexError):
+            pass
+ax.legend(bbox_to_anchor=(0, 1), loc='upper right', ncol=1)
+plt.show()
+print(results)
--- generate_samples.py
+++ generate_samples.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-freqs = [.3, .3, .05, .05, .05, .05, .05, .05, .05, .05]
+from Models import diff_features
+
+freqs = [.42, .3, .01, .05, .05, .05, .01, .01, .05, .05]
 
 
 def generate_alleles():
@@ -21,30 +23,32 @@ def generate_mixture(allele_pairs, mean_rfus):
     return np.sum(persons, axis=0)
 
 
-def generate_samples(n, is_binary, n_contributors=3, threshold=50, n_samples=2):
-    res = np.zeros((n, len(freqs) * n_samples))
+def generate_samples(n, is_binary, n_contributors=3, threshold = 50):
+    res = np.zeros((n, len(freqs) * 2))
     for i in range(n):
-        allele_pairs = [generate_alleles() for _ in range(n_samples*n_contributors)]
-        mean_rfus = np.random.randint(50, 1000, n_samples*n_contributors)
+        allele_pairs = [generate_alleles() for _ in range(2*n_contributors)]
+        mean_rfus = np.random.randint(50, 1000, 2*n_contributors)
         res[i, :len(freqs)] = generate_mixture(allele_pairs[:n_contributors], mean_rfus[:n_contributors])
         if i < n / 2:
-            res[i, len(freqs):len(freqs)*2] = generate_mixture(
-                allele_pairs[n_contributors:2*n_contributors-1] + [allele_pairs[0]], mean_rfus[n_contributors:2*n_contributors])
-            res[i, len(freqs)*2:] = generate_mixture(
-                allele_pairs[n_contributors*2:-1] + [allele_pairs[0]], mean_rfus[2*n_contributors:])
+            res[i, len(freqs):] = generate_mixture(
+                allele_pairs[n_contributors:-1] + [allele_pairs[0]], mean_rfus[n_contributors:])
         else:
-            res[i, len(freqs):len(freqs)*2] = generate_mixture(
-                allele_pairs[n_contributors:2*n_contributors], mean_rfus[n_contributors:2*n_contributors])
-            res[i, len(freqs)*2:] = generate_mixture(
-                allele_pairs[2*n_contributors:], mean_rfus[2*n_contributors:])
+            res[i, len(freqs):] = generate_mixture(
+                allele_pairs[n_contributors:], mean_rfus[n_contributors:])
     y = np.array([1] * int(n / 2) + [0] * int(n / 2))
     if is_binary:
         return 1*(res>threshold), y
     return res, y
 
+def generate_multiple_samples(n, is_binary, n_contributors=3, threshold=50, n_loci = 20):
+    samples = []
+    for i in range(n_loci):
+        samples.append(diff_features(generate_samples(n, is_binary, n_contributors, threshold)[0]))
+    return samples
+
 
 if __name__ == '__main__':
     allele_pairs = [generate_alleles() for _ in range(3)]
-    X, y  = generate_samples(10, False, 1, n_samples=3)
+    X, y  = generate_samples(10, False, 1)
     binary = 1 * (X > 50)
-    print(X.shape)
+    print(X)
